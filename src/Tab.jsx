b3fd2b441abfa0b024a5ac16a222816a411d8ca5
David Jaja <jajadavidjid@gmail.com>
2024-01-17 13:26:13 +0100

    image fix

--- src/Tab.jsx
+++ src/Tab.jsx
@@ -6,27 +6,27 @@ const petData = [
   {
     animal: "Cheetah",
     fact: "Cheetahs are the fastest land animals, capable of reaching speeds up to 75 mph.",
-    image: "../src/assets/6.svg",
+    image: "./assets/6.svg",
   },
   {
     animal: "Koala",
     fact: "Koalas sleep around 20 hours a day and are known for their eucalyptus diet.",
-    image: "../src/assets/3.svg",
+    image: "./assets/3.svg",
   },
   {
     animal: "Elephant",
     fact: "Elephants have the largest brains among land animals and demonstrate remarkable intelligence.",
-    image: "../src/assets/1.svg",
+    image: "./assets/1.svg",
   },
   {
     animal: "Zebra",
     fact: "Zebras have distinctive black and white stripes that act as a natural defense against predators.",
-    image: "../src/assets/7.svg",
+    image: "./assets/7.svg",
   },
   {
     animal: "Horse",
     fact: "Horses have excellent memory and are capable of recognizing human emotions.",
-    image: "../src/assets/5.svg",
+    image: "./assets/5.svg",
   },
 ];
 
